97a7c039b2d2c291a8fc35906ea830853bfc1927
java <java@bigdata>
2017-09-08 14:37:52 +0900

    first commit (config)

---
+++ src/main/java/com/example/SpringDiExample.java
@@ -0,0 +1,18 @@
+package com.example;
+
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+import com.example.model.Dept;
+
+public class SpringDiExample {
+	public static void main(String[] args) {
+		GenericXmlApplicationContext ctx = new GenericXmlApplicationContext("classpath:/spring/beans.xml");
+		
+		
+		Dept dept = ctx.getBean(Dept.class);
+		System.out.println(dept);
+		
+		ctx.close();
+		
+	}
+}
